43aed6401fb4e5911ce35eedeeae14dda07513e0
Drew McDermott <dmcdermott@edanalytics.org>
2016-03-06 12:15:20 -0600

    updates based on new data files

--- format_data_for_analysis.R
+++ format_data_for_analysis.R
@@ -16,9 +16,7 @@
   ea_start()
   
   # load packages
-  library(readxl)
   library(data.table)
-  library(eaanalysis)
 
 #############
 # set parms #
@@ -31,17 +29,14 @@
 # load data #
 #############
 
-  # load id xwalk
-  in_id_xwalk <- data.table(read_excel("X:/LFS-Education Outcomes/xwalk_child_id.xlsx"))
-  
   # load ed outcomes data
-  in_ed_outcomes_set <- fread("X:/LFS-Education Outcomes/DCFmatchedSample03012016.csv")
+  in_ed_outcomes_set <- fread("X:/LFS-Education Outcomes/raw_data/DCFmatchedSample03012016.csv")
   
   # load long ohc file
-  in_ohc_2012_long <- data.table(read_excel("X:/LFS-Education Outcomes/dcf_placement_2012.xlsx"))
+  in_ohc_2012 <- fread("X:/LFS-Education Outcomes/raw_data/dcf_placement_2012.csv")
   
-  # load wide ohc file
-  in_ohc_2012_wide <- data.table(read_excel("X:/LFS-Education Outcomes/dcf_wide_2012.xlsx"))
+  # load id xwalk
+  in_id_xwalk <- fread("X:/LFS-Education Outcomes/raw_data/xwalk_child_id.csv")
 
 ##################
 # general format #
@@ -50,26 +45,19 @@
   # copy raw files
   format_id_xwalk <- copy(in_id_xwalk)
   format_ed_outcomes <- copy(in_ed_outcomes_set)
-  format_ohc_2012  <- copy(in_ohc_2012_wide)
-  format_ohc_2012_long  <- copy(in_ohc_2012_long)
+  format_ohc_2012  <- copy(in_ohc_2012)
 
-  
   # convert var names to lowercase
-  setnames(format_id_xwalk, colnames(format_id_xwalk), tolower(colnames(format_id_xwalk)))
   setnames(format_ed_outcomes, colnames(format_ed_outcomes), tolower(colnames(format_ed_outcomes)))
-  setnames(format_ohc_2012, colnames(format_ohc_2012), tolower(colnames(format_ohc_2012)))
-  setnames(format_ohc_2012_long, colnames(format_ohc_2012_long), tolower(colnames(format_ohc_2012_long)))
 
-  # remove na rows from ohc data
-  format_id_xwalk <- subset(format_id_xwalk, !is.na(new_id))
-  format_ohc_2012 <- subset(format_ohc_2012, !is.na(child_id))
-  format_ohc_2012_long <- subset(format_ohc_2012_long, !is.na(child_id))
-
-##################
-# format ed data #
-##################
+###################
+# format ohc data #
+###################
   
-
+  # create list of vars to check on duplicates
+  vars_dup <- colnames(format_ohc_2012)
+  vars_nocheck <- c("case_type", "end_reason", "child_level_of_need", "days_plcmt_in_rpt_period")
+  vars_dup <- setdiff(vars_dup, vars_nocheck)
   
 ##################
 # format ed data #
