9c9806ffa85074fd2329f30df7ffdb510d516708
Drew McDermott <dmcdermott@edanalytics.org>
2016-03-07 14:24:13 -0600

    remove ed outcomes code

--- format_data_for_analysis.R
+++ format_data_for_analysis.R
@@ -28,9 +28,6 @@
 #############
 # load data #
 #############
-
-  # load ed outcomes data
-  in_ed_outcomes_set <- fread("X:/LFS-Education Outcomes/raw_data/DCFmatchedSample03012016.csv")
   
   # load long ohc file
   in_ohc_2012 <- fread("X:/LFS-Education Outcomes/raw_data/dcf_placement_2012.csv")
@@ -38,30 +35,19 @@
   # load id xwalk
   in_id_xwalk <- fread("X:/LFS-Education Outcomes/raw_data/xwalk_child_id.csv")
 
-##################
-# general format #
-##################
-  
-  # copy raw files
-  format_id_xwalk <- copy(in_id_xwalk)
-  format_ed_outcomes <- copy(in_ed_outcomes_set)
-  format_ohc_2012  <- copy(in_ohc_2012)
-
-  # convert var names to lowercase
-  setnames(format_ed_outcomes, colnames(format_ed_outcomes), tolower(colnames(format_ed_outcomes)))
-
 ###################
 # format ohc data #
 ###################
+  
+  # copy raw files
+  format_id_xwalk <- copy(in_id_xwalk)
+  format_ohc_2012  <- copy(in_ohc_2012)
   
   # create list of vars to check on duplicates
   vars_dup <- colnames(format_ohc_2012)
   vars_nocheck <- c("case_type", "end_reason", "child_level_of_need", "days_plcmt_in_rpt_period")
   vars_dup <- setdiff(vars_dup, vars_nocheck)
-  
-##################
-# format ed data #
-##################
+
   
   
 ##########
