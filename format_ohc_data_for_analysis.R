3849c6a8fea2254ad9070f36dddab54010f01984
Drew McDermott <dmcdermott@edanalytics.org>
2016-03-07 16:39:33 -0600

    update to broad duplicate subset, and current year

--- format_ohc_data_for_analysis.R
+++ format_ohc_data_for_analysis.R
@@ -35,19 +35,65 @@
   # load id xwalk
   in_id_xwalk <- fread("X:/LFS-Education Outcomes/raw_data/xwalk_child_id.csv")
 
-###################
-# format ohc data #
-###################
+############################
+# remove duplicate entries #
+############################
   
-  # copy raw files
-  format_id_xwalk <- copy(in_id_xwalk)
-  format_ohc_2012  <- copy(in_ohc_2012)
+  # copy raw file
+  sub_ohc_2012  <- copy(in_ohc_2012)
   
-  # create list of vars to check on duplicates
-  vars_dup <- colnames(format_ohc_2012)
-  vars_nocheck <- c("case_type", "end_reason", "child_level_of_need", "days_plcmt_in_rpt_period")
-  vars_dup <- setdiff(vars_dup, vars_nocheck)
+  # # create list of vars to check on duplicates
+  # vars_dup <- colnames(sub_ohc_2012)
+  # vars_nocheck <- c("site_region", "case_type", "child_person_type", "child_level_of_need", "provider_loc", "plcmt_with_sibling", 
+  #                   "days_plcmt_in_rpt_period") 
+  # vars_dup <- setdiff(vars_dup, vars_nocheck)
+  # 
+  # # subset based on duplicate vars list #brule
+  # sub_ohc_2012 <- ea_no_dups(sub_ohc_2012, vars_dup)
+  # 
+  # # sort based on child id, placement end date, and discharge variables (reverse sort discharge so "yes" appears first)
+  # setorder(sub_ohc_2012, child_id, plcmt_end_date, -discharge, discharge_reason, end_reason)
+  # 
+  # # remove discharge vars from no dup check
+  # vars_dup <- setdiff(vars_dup, c("discharge", "discharge_reason", "end_reason"))
+  # 
+  # # remove additional duplicates ignoring discharge vars #brule
+  # sub_ohc_2012 <- ea_no_dups(sub_ohc_2012, vars_dup)
+  
+  # remove duplicates based on id, and placement start and end date #brule
+  sub_ohc_2012 <- ea_no_dups(sub_ohc_2012, c("child_id", "plcmt_begin_date", "plcmt_end_date"))
+  
+##########################
+# subset to current year #
+##########################
+  
+  # create placement year vars
+  sub_ohc_2012[, plcmt_begin_year := as.numeric(ea_scan(plcmt_begin_date, 1, "-"))]
+  sub_ohc_2012[, plcmt_end_year := as.numeric(ea_scan(plcmt_end_date, 1, "-"))]
+  
+  # create flag for in placement year
+  sub_ohc_2012[, flag_in_year := 0]
+  sub_ohc_2012[plcmt_begin_date <= 2012 & plcmt_end_year >= 2012, flag_in_year := 1]
+  
+  # subset to placements from current year
+  sub_ohc_2012 <- subset(sub_ohc_2012, flag_in_year == 1)
 
+  
+
+  
+  
+
+
+
+  
+  
+  care_type
+days_plcmt_in_rpt_period
+child_level_of_need
+site_region
+end_reason
+discharge
+discharge_reason
   
   
 ##########
