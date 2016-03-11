610b9f6485d0fa8a103dbcfaff9036cd0e19a140
Drew McDermott <dmcdermott@edanalytics.org>
2016-03-10 21:26:48 -0600

    format stacked data and export

--- format_ohc_data_for_analysis.R
+++ format_ohc_data_for_analysis.R
@@ -1,8 +1,8 @@
 ######################################################################
 # notes:
-# - purpose:
-# - inputs:
-# - outputs:
+# - purpose: import, format, and stack individual year files, including consolidating ohc placements and adding dummy variables
+# - inputs: 5 ohc long files, 2008 - 2012
+# - outputs: consolidated set with all years stacked together
 # - keywords: #brule
 # - general:
 ######################################################################
@@ -24,6 +24,9 @@
   
   # output toggle
   p_opt_exp <- 0
+  
+  # load id xwalk
+  p_id_xwalk <- fread("X:/LFS-Education Outcomes/data/raw_data/xwalk_child_id.csv")
   
 ##################################
 # create list of files to import #
@@ -154,9 +157,8 @@
     
       # keep only child specific vars #NEED TO KEEP THIRD FRIDAY FLAG
       unique_child_set <- subset(unique_child_set, select = c(child_id, child_dob, child_gender, child_race, child_ethnicity, child_hispanic, 
-                                                              child_disability, disabilities, dcf_plcmt_type, fl_mntal_retardatn, fl_phys_disabled, 
-                                                              fl_vis_hearing_impr, fl_emotion_dstrbd, fl_othr_spc_care, fl_lrn_disability, region, 
-                                                              provider_county, removal_date, discharge_date, tpr_finalization_date, adoption_final_date))
+                                                              child_disability, disabilities, dcf_plcmt_type, region, provider_county, removal_date,
+                                                              discharge_date, tpr_finalization_date, adoption_final_date))
       
       # merge aggregate placement info with child info
       full_set <- ea_merge(unique_child_set, agg_plcmt_by_child, "child_id", opt_print = 0)
@@ -182,7 +184,99 @@
 
   # run function for full list of year files
   stacked_set <- func_format_stack(p_file_names)
+
+################################
+# create additional child vars #
+################################
   
+  # # create gender female flag
+  # stacked_set[, flag_female := 0]
+  # stacked_set[child_gender == "Female", flag_female := 1]
+  # 
+  # # create frequency of race / hispanic combinations
+  # a_freq_race <- ea_table(stacked_set, c("child_race", "child_hispanic"), opt_percent = 1)
+  # 
+  # # create race flags #brule
+  # stacked_set[, c("flag_amer_indian", "flag_asian", "flag_black", "flag_pac_island", "flag_white", "flag_other") := 0]
+  # stacked_set[child_race == "American Indian/Alaskan Native", flag_amer_indian := 1]
+  # stacked_set[child_race == "Asian", flag_asian := 1]
+  # stacked_set[child_race == "Black/African American", flag_black := 1]
+  # stacked_set[child_race == "Native Hawaiian/Other Pacific Islande", flag_pac_island := 1]
+  # stacked_set[child_race == "White", flag_white := 1]
+  # stacked_set[child_race == "Unable to Determine" | is.na(child_race), flag_other := 1]
+  # 
+  # # create hispanic flag
+  # stacked_set[, flag_hispanic := 0]
+  # stacked_set[child_hispanic == "Y", flag_hispanic := 1]
+  # 
+  # # create disability flag
+  # stacked_set[, flag_disability := 0]
+  # stacked_set[child_disability == "Y", flag_disability := 1]
+
+##############################
+# create additional ohc vars #
+##############################
+  
+  # create ohc begin and end years
+  stacked_set[, ohc_begin_year := as.numeric(ea_scan(removal_date, 1, "-"))]
+  stacked_set[, ohc_end_year := as.numeric(ea_scan(discharge_date, 1, "-"))]
+  
+  # create placement type dummies
+  stacked_set[, c("flag_fhome_rel", "flag_fhome_nrel", "flag_ghome", "flag_rcc", "flag_plcmt_other") := 0]
+  stacked_set[dcf_plcmt_type == "Foster Home (Relative)", flag_fhome_rel := 1]
+  stacked_set[dcf_plcmt_type == "Foster Home (Non-Relative)", flag_fhome_nrel := 1]
+  stacked_set[dcf_plcmt_type == "Group Home", flag_ghome := 1]
+  stacked_set[dcf_plcmt_type == "RCC", flag_rcc := 1]
+  stacked_set[is.na(dcf_plcmt_type), flag_plcmt_other := 1]
+  
+  # create region strings
+  p_reg_nc <- c("Vilas", "Oneida", "Forest", "Lincoln", "Langlade", "Marathon", "Wood", "Portage", "Adams")
+  p_reg_ne <- c("Florence", "Marinette", "Menominee", "Oconto", "Shawano", "Waupaca", "Outagamie", "Brown", "Kewaunee", "Door", "Waushara", 
+                "Winnebago", "Calumet", "Manitowoc", "Green Lake", "Fond Du Lac", "Sheboygan")
+  p_reg_nw <- c("Bayfield", "Ashland", "Iron", "Sawyer", "Price", "Taylor", "Douglas", "Burnett", "Washburn", "Polk", "Barron", "Rusk")
+  p_reg_se <- c("Walworth", "Racine", "Kenosha")
+  p_reg_s <- c("Marquette", "Richland", "Sauk", "Columbia", "Dodge", "Washington", "Ozaukee", "Grant", "Iowa", "Dane", "Jefferson", "Waukesha", 
+               "Lafayette", "Green", "Rock")
+  p_reg_w <- c("St Croix", "Dunn", "Chippewa", "Pierce", "Pepin", "Eau Claire", "Clark", "Buffalo", "Trempealeau", "Jackson", "La Crosse", 
+               "Monroe", "Juneau", "Vernon", "Crawford")
+
+  # create regions for missings #brule
+  stacked_set[is.na(region) & provider_county %in% p_reg_nc, region := "Northcentral"]
+  stacked_set[is.na(region) & provider_county %in% p_reg_ne, region := "Northeast"]
+  stacked_set[is.na(region) & provider_county %in% p_reg_nw, region := "Northwest"]
+  stacked_set[is.na(region) & provider_county %in% p_reg_se, region := "Southeast"]
+  stacked_set[is.na(region) & provider_county %in% p_reg_s, region := "South"]
+  stacked_set[is.na(region) & provider_county %in% p_reg_w, region := "West"]
+  stacked_set[is.na(region) & provider_county == "Milwaukee", region := "Milwaukee"]
+
+  # create region dummies
+  stacked_set[, c("flag_reg_mke", "flag_reg_nc", "flag_reg_ne", "flag_reg_nw", "flag_reg_s", "flag_reg_se", "flag_reg_w", "flag_reg_other") := 0]
+  stacked_set[region == "Milwaukee", flag_reg_mke := 1]
+  stacked_set[region == "Northcentral", flag_reg_nc := 1]
+  stacked_set[region == "Northeast", flag_reg_ne := 1]
+  stacked_set[region == "Northwest", flag_reg_nw := 1]
+  stacked_set[region == "South", flag_reg_s := 1]
+  stacked_set[region == "Southeast", flag_reg_se := 1]
+  stacked_set[region == "West", flag_reg_w := 1]
+  stacked_set[is.na(region), flag_reg_other := 1]
+  
+  
+######################
+# merge with id vars #
+######################
+  
+  # change id var names for merge
+  setnames(p_id_xwalk, c("new_id", "CHILD_ID"), c("child_id", "dpi_id"))
+  
+  # remove duplicates on child_id #brule
+  p_id_xwalk <- ea_no_dups(p_id_xwalk, "child_id")
+  
+  # merge on xwalk ids
+  out_set <- ea_merge(stacked_set, p_id_xwalk, "child_id", "x", opt_print = 0)
+  
+  # reorder cols
+  ea_colorder(out_set, c("dcf_year", "child_id", "dpi_id"))
+
 ##########
 # export #
 ##########
@@ -190,7 +284,7 @@
   # export
   if (p_opt_exp == 1) { 
     
-    ea_write( , ".csv")
+    ea_write(out_set, "X:/LFS-Education Outcomes/data/lfs_data/stacked_dcf_set.csv")
     
   }
 
