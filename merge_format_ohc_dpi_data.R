433fae47e80b0d1064e7dec7ffa5f79eedf5ccd1
Drew McDermott <dmcdermott@edanalytics.org>
2016-03-14 23:17:33 -0500

    begin edits to merge process

--- merge_format_ohc_dpi_data.R
+++ merge_format_ohc_dpi_data.R
@@ -30,7 +30,7 @@
 #############
   
   # load stacked ohc data
-  in_stacked_ohc <- fread("X:/LFS-Education Outcomes/data/lfs_data/stacked_dcf_set.csv")
+  in_stacked_ohc <- fread("X:/LFS-Education Outcomes/data/lfs_data/lst_set_agg_plcmt.csv")
   
   # load ed outcomes data
   in_stacked_dpi <- fread("X:/LFS-Education Outcomes/data/raw_data/DCFmatchedSample03012016.csv")
@@ -42,11 +42,11 @@
   # copy raw file
   format_stacked_dpi <- copy(in_stacked_dpi)
   
-  # convert variable names to lowercase
-  setnames(format_stacked_dpi, colnames(format_stacked_dpi), tolower(colnames(format_stacked_dpi)))
-  
   # remove exact duplicates #brule
   format_stacked_dpi <- ea_no_dups(format_stacked_dpi, opt_key_all = 1)
+  
+  # convert variable names to lowercase
+  setnames(format_stacked_dpi, colnames(format_stacked_dpi), tolower(colnames(format_stacked_dpi)))
 
   # sort based on child id, school year, and test scores (to remove rows with missing scores first) #brule
   setorder(format_stacked_dpi, lds_student_key, school_year, math_kce_scale_score, rdg_kce_scale_score, na.last = TRUE)
@@ -58,14 +58,11 @@
 # format dpi data #
 ###################
     
-  # create dcf year variable
-  format_stacked_dpi[, dcf_year := paste0("20", ea_scan(school_year, 2, "-"))]
-  
-  # rename child id var
-  setnames(format_stacked_dpi, "child_id", "dpi_id")
+  # create lst_plcmt_yr var to merge
+  format_stacked_dpi[, lst_plcmt_yr := as.numeric(paste0("20", ea_scan(school_year, 2, "-")))]
   
   # subset to dpi data to years in ohc file
-  sub_dpi_for_merge <- subset(format_stacked_dpi, as.numeric(dcf_year) >= 2008 & as.numeric(dcf_year) <= 2012)
+  sub_dpi_for_merge <- subset(format_stacked_dpi, lst_plcmt_yr >= 2007 & lst_plcmt_yr <= 2012)
 
 ##################################
 # subset and merge with ohc data #
@@ -75,7 +72,7 @@
   ohc_data_for_merge <- copy(in_stacked_ohc)
   
   # merge data sets
-  merged_set <- ea_merge(ohc_data_for_merge, sub_dpi_for_merge, c("dpi_id", "dcf_year"), "x")
+  merged_set <- ea_merge(ohc_data_for_merge, sub_dpi_for_merge, c("child_id", "lst_plcmt_yr"), "x")
   
 ##########
 # export #
