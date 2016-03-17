fcf26d9d1dcd820d39244d81b539cd17a66eedca
Drew McDermott <dmcdermott@edanalytics.org>
2016-03-17 11:25:00 -0500

    updates about beginning code switches

--- analysis_summarize_ohc_data.R
+++ analysis_summarize_ohc_data.R
@@ -39,9 +39,15 @@
   # copy raw data
   sub_ohc_data <- copy(in_stacked_ohc)
   
+  # set placement vars to numeric
+  sub_ohc_data[, 11:15] <- lapply(sub_ohc_data[, 11:15, with = FALSE], as.numeric)
+
   # remove entries with 0 for placement days in acad year #brule
-  sub_ohc_data <- subset(sub_ohc_data, as.numeric(plcmt_days_acad_year) != 0)
+  sub_ohc_data <- subset(sub_ohc_data, plcmt_days_acad_year != 0)
   
+  # set placement days longer than 365 to 365 #brule
+  sub_ohc_data[plcmt_days_acad_year > 365, plcmt_days_acad_year := 365]
+
   # create set that has one row per student to calc overall numbers
   setorder(sub_ohc_data, child_id, -acad_year)
   sub_ohc_data_unique <- ea_no_dups(sub_ohc_data, "child_id")
@@ -56,9 +62,6 @@
   # melt ohc data long to summarize
   long_data_overall <- melt.data.table(sub_ohc_data_unique, id.vars = c("child_id", "removal_date"))
   
-  # set value var to numeric
-  long_data_overall[, value := as.numeric(value)]
-  
   # create removal year var
   long_data_overall[, removal_year := year(removal_date)]
   
@@ -66,28 +69,28 @@
   setorder(long_data_overall, variable, removal_year)
   
   # calc ohc stats overall
-  a_overall_stats <- long_data_overall[!is.na(value), list(n_obs = length(value),
-                                                       min = min(value),
-                                                       q25 = quantile(value, .25),
-                                                       q50 = quantile(value, .5),
-                                                       q75 = quantile(value, .75),
-                                                       max = max(value),
-                                                       mean = round(mean(value), 2),
-                                                       var = round(var(value), 2),
-                                                       sd = round(sd(value), 2)), 
-                                   by = c("variable")]
+  a_summ_overall <- long_data_overall[!is.na(value), list(n_obs = length(value),
+                                                           min = min(value),
+                                                           q25 = quantile(value, .25),
+                                                           q50 = quantile(value, .5),
+                                                           q75 = quantile(value, .75),
+                                                           max = max(value),
+                                                           mean = round(mean(value), 2),
+                                                           var = round(var(value), 2),
+                                                           sd = round(sd(value), 2)), 
+                                      by = c("variable")]
 
   # calc ohc stats overall, by removal year
-  a_overall_stats_yr <- long_data_overall[!is.na(value), list(n_obs = length(value),
-                                                              min = min(value),
-                                                              q25 = quantile(value, .25),
-                                                              q50 = quantile(value, .5),
-                                                              q75 = quantile(value, .75),
-                                                              max = max(value),
-                                                              mean = round(mean(value), 2),
-                                                              var = round(var(value), 2),
-                                                              sd = round(sd(value), 2)),
-                                          by = c("variable", "removal_year")]
+  a_summ_overall_by_yr <- long_data_overall[!is.na(value), list(n_obs = length(value),
+                                                                min = min(value),
+                                                                q25 = quantile(value, .25),
+                                                                q50 = quantile(value, .5),
+                                                                q75 = quantile(value, .75),
+                                                                max = max(value),
+                                                                mean = round(mean(value), 2),
+                                                                var = round(var(value), 2),
+                                                                sd = round(sd(value), 2)),
+                                            by = c("variable", "removal_year")]
 
 #####################################
 # produce summary tables by acad yr #
@@ -98,19 +101,16 @@
                                                      num_plcmt_acad_yr, plcmt_days_acad_year))
   
   # change removal date to removal year
-  sub_ohc_acad_yr[, removal_date := as.character(year(removal_date))]
+  sub_ohc_acad_yr[, removal_date := as.numeric(year(removal_date))]
 
   # melt ohc data long to summarize
   long_data_acad <- melt.data.table(sub_ohc_acad_yr, id.vars = c("child_id", "acad_year"))
   
-  # set value var to numeric
-  long_data_acad[, value := as.numeric(value)]
+  # subset to only acad year vars
+  sub_long_data_acad <- subset(long_data_acad, variable == "num_plcmt_acad_yr" | variable == "plcmt_days_acad_year")
   
-  # sort by variable, removal year
-  setorder(long_data_acad, variable, acad_year)
-  
-  # calc summary stats on long file, with by group
-  a_acad_yr_stats <- long_data_acad[!is.na(value), list(n_obs = length(value),
+  # calc acad yr stats overall
+  a_summ_acad <- sub_long_data_acad[!is.na(value), list(n_obs = length(value),
                                                        min = min(value),
                                                        q25 = quantile(value, .25),
                                                        q50 = quantile(value, .5),
@@ -119,7 +119,24 @@
                                                        mean = round(mean(value), 2),
                                                        var = round(var(value), 2),
                                                        sd = round(sd(value), 2)), 
-                                    by = c("variable", "acad_year")]
+                                    by = c("variable")]
+  
+  # sort by variable, removal year
+  setorder(long_data_acad, variable, acad_year)
+
+  # calc ohc stats by acad year
+  a_summ_acad_by_yr <- long_data_acad[!is.na(value), list(n_obs = length(value),
+                                                           min = min(value),
+                                                           q25 = quantile(value, .25),
+                                                           q50 = quantile(value, .5),
+                                                           q75 = quantile(value, .75),
+                                                           max = max(value),
+                                                           mean = round(mean(value), 2),
+                                                           var = round(var(value), 2),
+                                                           sd = round(sd(value), 2)), 
+                                      by = c("variable", "acad_year")]
+  
+  
 #####################
 # plot summary info #
 #####################
@@ -172,10 +189,17 @@
                                              title = paste0("Growth on Comparable Measures by District: ", sy_label)) + 
                                         plot_attributes +
                                         facet_wrap(~district_code, ncol = 4)
-##########
-# export #
-##########
-
+#####################
+# format and export #
+#####################
+  
+  # subset acad yr table to export
+  out_summ_acad_by_yr <- subset(a_summ_acad_by_yr, as.numeric(acad_year) > 2007 & as.numeric(acad_year) < 2013)
+  
+  # sort output set
+  setorder(out_summ_acad_by_yr, acad_year, variable)
+  
+  
   # set height and width of plots
   p_height <- 28
   p_width <- 28
@@ -184,8 +208,10 @@
   # export
   if (p_opt_exp == 1) { 
     
-    ea_write( , ".csv")
-    
+    ea_write(a_summ_overall, "X:/LFS-Education Outcomes/qc/summary_stats_ohc_overall.csv")
+    ea_write(a_summ_acad, "X:/LFS-Education Outcomes/qc/summary_stats_acad.csv")
+    ea_write(out_summ_acad_by_yr, "X:/LFS-Education Outcomes/qc/summary_stats_acad_by_yr.csv")
+
     ggsave("X:/LFS-Education Outcomes/qc/hist_ohc_days_overall.png", plot = plot_hist_ohc_days, width = p_width, height = p_height, units = "cm")
     ggsave("X:/LFS-Education Outcomes/qc/hist_ohc_days_by_acad_yr.png", plot = plot_hist_ohc_days_byr, 
            width = p_width, height = p_height, units = "cm")
