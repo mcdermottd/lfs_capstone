88bf9bd27883d9248965145cd51b04e49c63558d
Drew McDermott <dmcdermott@edanalytics.org>
2016-03-17 18:13:49 -0500

    re-do merge (still not working!!)

--- merge_format_ohc_dpi_data.R
+++ merge_format_ohc_dpi_data.R
@@ -30,14 +30,37 @@
 #############
   
   # load stacked ohc data
-  in_stacked_ohc <- fread("X:/LFS-Education Outcomes/data/lfs_data/lst_set_agg_plcmt.csv")
+  in_stacked_ohc <- fread("X:/LFS-Education Outcomes/data/lfs_data/stacked_acad_yr_set.csv", colClasses = "character")
   
   # load ed outcomes data
   in_stacked_dpi <- fread("X:/LFS-Education Outcomes/data/raw_data/DCFmatchedSample03012016.csv")
+  
+  # load id xwalk
+  in_id_xwalk <- fread("X:/LFS-Education Outcomes/data/raw_data/xwalk_child_id.csv")
 
-###################################
-# remove duplicates from dpi data #
-###################################
+############################
+# format ohc data to merge #
+############################
+  
+  # copy raw files
+  format_stacked_ohc <- copy(in_stacked_ohc)
+  format_id_xwalk <- copy(in_id_xwalk)
+  
+  # rename id vars in xwalk
+  setnames(format_id_xwalk, c("new_id", "CHILD_ID"), c("child_id", "merge_id"))
+  
+  # remove id duplicates #brule
+  format_id_xwalk <- ea_no_dups(format_id_xwalk, "child_id")
+  
+  # format child id var to merge
+  format_id_xwalk[, child_id := as.character(child_id)]
+  
+  # merge with ohc data
+  format_stacked_ohc <- ea_merge(format_id_xwalk, format_stacked_ohc, "child_id", "y")
+  
+###############################################
+# remove duplicates, format dpi data to merge #
+###############################################
   
   # copy raw file
   format_stacked_dpi <- copy(in_stacked_dpi)
@@ -54,21 +77,32 @@
   # remove duplicates based on child_id and school_year #brule
   format_stacked_dpi <- ea_no_dups(format_stacked_dpi, c("lds_student_key", "school_year"))
   
-  # rename child id var
+  # create academic year var for merge
+  format_stacked_dpi[, acad_year := as.character(paste0("20", ea_scan(school_year, 2, "-")))]
+
+  # rename child id var for merge
   setnames(format_stacked_dpi, "child_id", "merge_id")
 
 #######################
 # merge with ohc data #
 #######################
   
-  # copy raw ohc file
-  ohc_data_for_merge <- copy(in_stacked_ohc)
+  # merge ohc and dpi data, on merge id and academic year var
+  merged_set <- ea_merge(format_stacked_ohc, format_stacked_dpi, c("merge_id", "acad_year"), "x")
+
+  # create merge flag variable
+  merged_set[, flag_merge := 0]
+  merged_set[!is.na(lds_student_key), flag_merge := 1]
   
-  # merge on academic outcomes to ohc data
-  merged_output <- ea_merge(ohc_data_for_merge, format_stacked_dpi, "merge_id", "both", opt_out_mismatches = 1)
+  # create frequency of merge flag by year
+  qc_merge_rate <- ea_table(merged_set, c("acad_year", "flag_merge"))
+
+  # subset to unmerged records
+  unmerged_set <- subset(merged_set, flag_merge == 0, select = c(child_id, merge_id, lds_student_key))
   
-  # copy merged set
-  ohc_dpi_data <- copy(merged_output$out_merged_data)
+  # remove dups to create unique unmerged ids
+  unmerged_set <- ea_no_dups(unmerged_set, "merge_id", opt_print = 0)
+  
   
   # remove ohc demo vars, coming from dpi data #brule
   ohc_dpi_data <- subset(ohc_dpi_data, select = -c(child_gender, child_race, child_ethnicity, child_hispanic, child_disability, disabilities, 
@@ -94,6 +128,9 @@
   if (p_opt_exp == 1) { 
     
     ea_write(full_set, "X:/LFS-Education Outcomes/data/lfs_data/combined_dpi_dcf_set.csv")
+    
+    ea_write(unmerged_set, "X:/LFS-Education Outcomes/qc/unmerged_id_set.csv")
+    ea_write(qc_merge_rate, "X:/LFS-Education Outcomes/qc/merge_rates_by_yr.csv")
     
   }
 
