b3758c68371906baae62c57c6f689fdad36e7e64
Drew McDermott <dmcdermott@edanalytics.org>
2016-04-07 21:23:38 -0500

    began making update based on new ohc data set

--- format_ohc_data_for_analysis.R
+++ format_ohc_data_for_analysis.R
@@ -22,124 +22,53 @@
 #############
 # set parms #
 #############
-    
-  # load id xwalk
-  p_id_xwalk <- fread("X:/LFS-Education Outcomes/data/raw_data/xwalk_child_id.csv")
-  
+
   # output toggle
-  p_opt_exp <- 1
+  p_opt_exp <- 0
 
-##################################
-# create list of files to import #
-##################################
+#############
+# load data #
+#############
   
-  # store raw directory path
-  p_dir_raw <- "X:/LFS-Education Outcomes/data/raw_data/"
+  # load long ohc file
+  in_ohc_data <- fread("X:/LFS-Education Outcomes/data/raw_data/dcf_long.csv", na.strings = "")
+
+#######################
+# format raw ohc data #
+#######################
   
-  # import names of files
-  p_file_names <- list.files(p_dir_raw)
-  p_file_names <- grep("placement", p_file_names, value = TRUE)
+  # copy raw data file
+  format_ohc_data  <- copy(in_ohc_data)
 
-###################################
-# format and stack dcf year files #
-###################################
-
-  # define function to open data
-  func_format_stack <- function(x_file) {
-
-    # start file list
-    file_list <- list()
-
-    # for file in path
-    for (m_file in x_file) {
-      
-      print(m_file)
-
-    #############
-    # load data #
-    #############
-      
-      # load long ohc file
-      in_ohc_data <- fread(paste0(p_dir_raw, m_file))
-      
-      # add file to out list
-      file_list[[m_file]] <- in_ohc_data
-    }
-
-    ######################################
-    # stack and return full set of files #
-    ######################################
-
-    # stack all files
-    out_file <- rbindlist(file_list, fill = TRUE, use.names = TRUE)
-
-    # return the stacked file
-    return(out_file)
-  }
-
-  # run function for full list of year files
-  stacked_set <- func_format_stack(p_file_names)
- 
-#############################################
-# remove exact duplicates from stacked data #
-#############################################
-  
-  # copy stacked file
-  ohc_data_full  <- copy(stacked_set)
-
-  # remove all exact duplicates from stacked data
-  ohc_data_full <- ea_no_dups(ohc_data_full, opt_key_all = 1) 
-
-###################################################
-# remove placements with the same start/end dates #
-###################################################
-  
   # change var names to lowercase
-  setnames(ohc_data_full, colnames(ohc_data_full), tolower(colnames(ohc_data_full)))
-  
-  # sort based on child id, placement dates, and end reason (end reason = NA appears last) #brule
-  setorder(ohc_data_full, child_id, plcmt_begin_date, plcmt_end_date, end_reason, na.last = TRUE)
-  
+  setnames(format_ohc_data, colnames(format_ohc_data), tolower(colnames(format_ohc_data)))
+
   # remove duplicates based on id, and placement start and end date #brule
-  ohc_data_no_dups <- ea_no_dups(ohc_data_full, c("child_id", "plcmt_begin_date", "plcmt_end_date"))
+  ohc_data_no_dups <- ea_no_dups(format_ohc_data, c("child_id", "plcmt_begin_date", "plcmt_end_date"))
   
-######################################################
-# calc total number of ohc days / days per placement #
-######################################################
+  # fill in December 31, 2014 for placements missing end date or discharge date #brule
+  ohc_data_no_dups[is.na(plcmt_end_date), plcmt_end_date := "31DEC2014"]
+  ohc_data_no_dups[is.na(discharge_date), discharge_date := "31DEC2014"]
 
-  # calc number of ohc days and plcmt days
-  ohc_data_no_dups[, ohc_days_tot := as.numeric(as.duration(ymd(discharge_date) - ymd(removal_date))) / 86400]
-  ohc_data_no_dups[, plcmt_days := as.numeric(as.duration(ymd(plcmt_end_date) - ymd(plcmt_begin_date))) / 86400]
+  # change format of date variables
+  format_ohc_data[, lfs]
+  
+  # calc number of ohc days and placement days
+  ohc_data_no_dups[, ohc_days := as.numeric(as.duration(dmy(discharge_date) - dmy(removal_date))) / 86400]
+  ohc_data_no_dups[, plcmt_days := as.numeric(as.duration(dmy(plcmt_end_date) - dmy(plcmt_begin_date))) / 86400]
 
-  # remove placement = 0 days #brule #NEED TO FIGURE THIS OUT WITH DCF
-  ohc_data_no_dups <- subset(ohc_data_no_dups, plcmt_days > 0)
-
-#######################################
-# aggregate total placements by child #
-#######################################
-
-  # aggregate placements by child
-  agg_plcmt <- ohc_data_no_dups[, list(num_plcmt_tot = .N,
-                                       plcmt_days_tot = sum(plcmt_days)),
-                                by = "child_id"]
-
-  # take average number of placement and total placement days
-  a_avg_plcmt <- agg_plcmt[, list(avg_num_plcmt = mean(num_plcmt_tot),
-                                  avg_plcmt_days = mean(plcmt_days_tot, na.rm = TRUE))]
-
-#############################
-# create academic year vars #
-#############################
-
-  # create school year plcmt vars #brule
-  ohc_data_no_dups[, plcmt_begin_syear := ifelse(month(plcmt_begin_date) > 5, year(ymd(plcmt_begin_date) + years(1)), year(plcmt_begin_date))]
-  ohc_data_no_dups[, plcmt_end_syear := ifelse(month(plcmt_end_date) > 5, year(ymd(plcmt_end_date) + years(1)), year(plcmt_end_date))]
+  # create school year for placement begin date, if month is after may, year = next academic year #brule
+  ohc_data_no_dups[, plcmt_begin_syear := ifelse(month(dmy(plcmt_begin_date)) > 5, year(dmy(plcmt_begin_date) + years(1)), 
+                                                 year(dmy(plcmt_begin_date)))]
+  
+  # create school year for placement end date, if month is after may, year = next academic year #brule
+  ohc_data_no_dups[, plcmt_end_syear := ifelse(month(dmy(plcmt_end_date)) > 5, year(dmy(plcmt_end_date) + years(1)), year(dmy(plcmt_end_date)))]
   
 #########################################
 # loop over placement years in data set #
 #########################################  
   
-  # create set of placement start and end dates
+  # create set of placement start years
   plcmt_dates <- subset(ohc_data_no_dups, select = plcmt_begin_syear)
   
   # subset to unique placement years
@@ -162,18 +91,18 @@
     #################################################################
 
       # create set of placements that spans placement year
-      acad_yr_set <- subset(ohc_data_no_dups, plcmt_begin_syear <= m_year & plcmt_end_date >= m_year)
+      acad_yr_set <- subset(ohc_data_no_dups, plcmt_begin_syear <= m_year &  plcmt_end_syear >= m_year)
       
-      # adj placement start date if in prior years #brule
+      # create adj start date (June 1) if placement begins prior to academic year #brule
       acad_yr_set[, adj_plcmt_begin_date := plcmt_begin_date]
-      acad_yr_set[plcmt_begin_syear != m_year, adj_plcmt_begin_date := paste0((m_year - 1), "-06-01")]
+      acad_yr_set[plcmt_begin_syear != m_year, adj_plcmt_begin_date := paste0("01JUN", as.character(m_year - 1))]
       
-      # adj placement end date if in subsequent years #brule
+      # create adj end date (May 31) if placement ends in subsequent years #brule
       acad_yr_set[, adj_plcmt_end_date := plcmt_end_date]
-      acad_yr_set[plcmt_end_syear != m_year, adj_plcmt_end_date := paste0(m_year, "-05-31")]
+      acad_yr_set[plcmt_end_syear != m_year, adj_plcmt_end_date := paste0("31MAY", as.character(m_year))]
   
       # calc number of plcmt days (in academic year)
-      acad_yr_set[, plcmt_days_acad := as.numeric(as.duration(ymd(adj_plcmt_end_date) - ymd(adj_plcmt_begin_date))) / 86400]
+      acad_yr_set[, plcmt_days_acad := as.numeric(as.duration(dmy(adj_plcmt_end_date) - dmy(adj_plcmt_begin_date))) / 86400]
 
       # aggregate placements by child by year
       agg_acad_plcmt <- acad_yr_set[, list(num_plcmt_acad_yr = .N,
@@ -185,7 +114,7 @@
     ##################################################
       
       # sort based on child id and placement start and end dates
-      setorder(acad_yr_set, child_id, adj_plcmt_begin_date, adj_plcmt_end_date)
+      setorder(acad_yr_set, child_id, dmy(adj_plcmt_begin_date), dmy(adj_plcmt_end_date))
       
       # number placements for casting
       acad_yr_set[, num_plcmt := seq_len(.N), by = child_id]
@@ -237,6 +166,22 @@
   
   # stack all files
   stacked_acad_yr_data <- rbindlist(file_list, fill = TRUE, use.names = TRUE)
+
+
+#######################################
+# aggregate total placements by child #
+#######################################
+
+  # aggregate placements by child
+  agg_plcmt <- ohc_data_no_dups[, list(num_plcmt_tot = .N,
+                                       plcmt_days_tot = sum(plcmt_days)),
+                                by = "child_id"]
+
+  # take average number of placement and total placement days
+  a_avg_plcmt <- agg_plcmt[, list(avg_num_plcmt = mean(num_plcmt_tot),
+                                  avg_plcmt_days = mean(plcmt_days_tot, na.rm = TRUE))]
+  
+  
   
 ##########
 # export #
