12b51a5dc0ad2f20c9e98b75b14a65bbce603ea9
Drew McDermott <dmcdermott@edanalytics.org>
2016-04-27 14:35:44 -0500

    update reg_ convention to lm_

--- analysis_academic_outcomes.R
+++ analysis_academic_outcomes.R
@@ -17,6 +17,10 @@
 
   # load packages
   library(stargazer)
+  library(sandwich)
+  library(lmtest)
+  library(plm)
+  library(multiwayvcov)
   library(data.table)
 
 #############
@@ -24,7 +28,7 @@
 #############
 
   # output toggle
-  p_opt_exp <- 1
+  p_opt_exp <- 0
 
 #############
 # load data #
@@ -124,92 +128,116 @@
                                                        avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
                                                        sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
                                                by = c("flag_cur_plcmt", "flag_prior_plcmt", "lf_region")]
-
+  
 #############################
 # set regression parameters #
 #############################
   
   # set student control vars
-  reg_student_controls <- c("age_in_years_cd", "d_male", "d_elp", "d_sped", "d_frl", "d_race_black", "d_race_hispanic", "d_race_asian", 
+  lm_student_controls <- c("age_in_years_cd", "d_male", "d_elp", "d_sped", "d_frl", "d_race_black", "d_race_hispanic", "d_race_asian", 
                             "d_race_indian")
   
   # set school covariates
-  reg_sch_controls <- c("sch_pupil_count", "sch_frl_scaled", "sch_sped_scaled", "sch_elp_scaled", "sch_non_white_scaled", "sch_removal_scaled",
+  lm_sch_controls <- c("sch_pupil_count", "sch_frl_scaled", "sch_sped_scaled", "sch_elp_scaled", "sch_non_white_scaled", "sch_removal_scaled",
                         "sch_mean_math_z_score", "sch_mean_rdg_z_score")
   
   # set year dummies
-  reg_dummies_yr <- c("d_acad_year_2009", "d_acad_year_2010", "d_acad_year_2011", "d_acad_year_2012")
+  lm_dummies_yr <- c("d_acad_year_2009", "d_acad_year_2010", "d_acad_year_2011", "d_acad_year_2012")
   
   # set grade dummies
-  reg_dummies_grade <- c("d_grade_09", "d_grade_10", "d_grade_11", "d_grade_12")
+  lm_dummies_grade <- c("d_grade_09", "d_grade_10", "d_grade_11", "d_grade_12")
 
   # combine for full set of controls
-  reg_controls_full <- paste(c(reg_student_controls, reg_sch_controls, reg_dummies_yr, reg_dummies_grade), collapse = " + ")
-  reg_controls_wkce <- paste(c(reg_student_controls, reg_sch_controls, reg_dummies_yr, "d_grade_09"), collapse = " + ")
-
+  lm_controls_full <- paste(c(lm_student_controls, lm_sch_controls, lm_dummies_yr, lm_dummies_grade), collapse = " + ")
+  lm_controls_wkce <- paste(c(lm_student_controls, lm_sch_controls, lm_dummies_yr, "d_grade_09"), collapse = " + ")
+  
+#################################
+# define SE clustering function #
+#################################
+  
+  func_cluster <- function(dat, fm, cluster) {
+    M <- length(unique(cluster))
+    N <- length(cluster)
+    K <- fm$rank
+    dfc <- (M / (M - 1)) * ((N - 1) / (N - K))
+    uj  <- apply(estfun(fm), 2, function(x) tapply(x, cluster, sum));
+    vcovCL <- dfc * sandwich(fm, meat = crossprod(uj) / N)
+    coeftest(fm, vcovCL)
+  }
+  
 ############################
 # regressions - attendence #
 ############################
 
+  sub_attend <- subset(analysis_set_hs, !is.na(att_rate_wi))
+  
   # reg: attendance on ohc flags and controls
-  reg_formula <- paste("att_rate_wi ~ flag_cur_plcmt + flag_prior_plcmt + ", reg_controls_full)
-  reg_attend_ohc <- lm(reg_formula, data = analysis_set_hs)
+  lm_formula <- paste("att_rate_wi ~ flag_cur_plcmt + flag_prior_plcmt + ", lm_controls_full)
+  lm_attend_ohc <- lm(lm_formula, data = analysis_set_hs)
 
   # reg: attendance on number of placements
-  reg_formula <- paste("att_rate_wi ~ lf_n_plcmt_acad + flag_prior_plcmt + ", reg_controls_full)
-  reg_attend_n_plcmt <- lm(reg_formula, data = analysis_set_hs)
+  lm_formula <- paste("att_rate_wi ~ lf_n_plcmt_acad + flag_prior_plcmt + ", lm_controls_full)
+  lm_attend_n_plcmt <- lm(lm_formula, data = analysis_set_hs)
 
   # reg: attendance on total placement days
-  reg_formula <- paste("att_rate_wi ~ tot_plcmt_days_acad + flag_prior_plcmt + ", reg_controls_full)
-  reg_attend_plcmt_days <- lm(reg_formula, data = analysis_set_hs)
+  lm_formula <- paste("att_rate_wi ~ tot_plcmt_days_acad + flag_prior_plcmt + ", lm_controls_full)
+  lm_attend_plcmt_days <- lm(lm_formula, data = analysis_set_hs)
 
+  
+    pm1 <- plm(as.formula(lm_formula), data = analysis_set_hs, model = "pooling")
+
+
+  
+  ztest <- lm(lm_formula, data = sub_attend)
+  lm_attend_ohc_cl <- func_cluster(sub_attend, ztest, sub_attend$sch_acctbl_code_cd)
+  
 ##########################
 # regressions - removals #
 ##########################
   
   # reg: removals on ohc flags and controls
-  reg_formula <- paste("days_removed_os ~ flag_cur_plcmt + flag_prior_plcmt + ", reg_controls_full)
-  reg_remove_ohc <- lm(reg_formula, data = analysis_set_hs)
+  lm_formula <- paste("days_removed_os ~ flag_cur_plcmt + flag_prior_plcmt + ", lm_controls_full)
+  lm_remove_ohc <- lm(lm_formula, data = analysis_set_hs)
 
   # reg: removals on number of placements
-  reg_formula <- paste("days_removed_os ~ lf_n_plcmt_acad + flag_prior_plcmt + ", reg_controls_full)
-  reg_remove_n_plcmt <- lm(reg_formula, data = analysis_set_hs)
+  lm_formula <- paste("days_removed_os ~ lf_n_plcmt_acad + flag_prior_plcmt + ", lm_controls_full)
+  lm_remove_n_plcmt <- lm(lm_formula, data = analysis_set_hs)
 
   # reg: removals on total placement days
-  reg_formula <- paste("days_removed_os ~ tot_plcmt_days_acad + flag_prior_plcmt + ", reg_controls_full)
-  reg_remove_plcmt_days <- lm(reg_formula, data = analysis_set_hs)
+  lm_formula <- paste("days_removed_os ~ tot_plcmt_days_acad + flag_prior_plcmt + ", lm_controls_full)
+  lm_remove_plcmt_days <- lm(lm_formula, data = analysis_set_hs)
 
 ###########################
 # regressions - wkce math #
 ###########################
   
   # reg: wkce math on ohc flag and controls
-  reg_formula <- paste("nxt_zscore_math_kce ~ flag_cur_plcmt + flag_prior_plcmt + ", reg_controls_wkce)
-  reg_kce_math_ohc <- lm(reg_formula, data = analysis_set_wkce)
+  lm_formula <- paste("nxt_zscore_math_kce ~ flag_cur_plcmt + flag_prior_plcmt + ", lm_controls_wkce)
+  lm_kce_math_ohc <- lm(lm_formula, data = analysis_set_wkce)
   
   # reg: wkce math on number of placements
-  reg_formula <- paste("nxt_zscore_math_kce ~ lf_n_plcmt_acad + flag_prior_plcmt + ", reg_controls_wkce)
-  reg_kce_math_n_plcmt <- lm(reg_formula, data = analysis_set_wkce)
+  lm_formula <- paste("nxt_zscore_math_kce ~ lf_n_plcmt_acad + flag_prior_plcmt + ", lm_controls_wkce)
+  lm_kce_math_n_plcmt <- lm(lm_formula, data = analysis_set_wkce)
 
   # reg: wkce math on total placement days
-  reg_formula <- paste("nxt_zscore_math_kce ~ tot_plcmt_days_acad + flag_prior_plcmt + ", reg_controls_wkce)
-  reg_kce_math_plcmt_days <- lm(reg_formula, data = analysis_set_wkce)
+  lm_formula <- paste("nxt_zscore_math_kce ~ tot_plcmt_days_acad + flag_prior_plcmt + ", lm_controls_wkce)
+  lm_kce_math_plcmt_days <- lm(lm_formula, data = analysis_set_wkce)
 
 ##############################
 # regressions - wkce reading #
 ##############################
   
   # reg: wkce reading on ohc flag and controls
-  reg_formula <- paste("nxt_zscore_rdg_kce ~ flag_cur_plcmt + flag_prior_plcmt + ", reg_controls_wkce)
-  reg_kce_rdg_ohc <- lm(reg_formula, data = analysis_set_wkce)
+  lm_formula <- paste("nxt_zscore_rdg_kce ~ flag_cur_plcmt + flag_prior_plcmt + ", lm_controls_wkce)
+  lm_kce_rdg_ohc <- lm(lm_formula, data = analysis_set_wkce)
   
   # reg: wkce reading on number of placements
-  reg_formula <- paste("nxt_zscore_rdg_kce ~ lf_n_plcmt_acad + flag_prior_plcmt + ", reg_controls_wkce)
-  reg_kce_rdg_n_plcmt <- lm(reg_formula, data = analysis_set_wkce)
+  lm_formula <- paste("nxt_zscore_rdg_kce ~ lf_n_plcmt_acad + flag_prior_plcmt + ", lm_controls_wkce)
+  lm_kce_rdg_n_plcmt <- lm(lm_formula, data = analysis_set_wkce)
 
   # reg: wkce reading on total placement days
-  reg_formula <- paste("nxt_zscore_rdg_kce ~ tot_plcmt_days_acad + flag_prior_plcmt + ", reg_controls_wkce)
-  reg_kce_rdg_plcmt_days <- lm(reg_formula, data = analysis_set_wkce)
+  lm_formula <- paste("nxt_zscore_rdg_kce ~ tot_plcmt_days_acad + flag_prior_plcmt + ", lm_controls_wkce)
+  lm_kce_rdg_plcmt_days <- lm(lm_formula, data = analysis_set_wkce)
   
 #####################
 # format and export #
@@ -246,71 +274,71 @@
     ea_write(a_acad_outcomes_by_region, paste0(p_dir_out, "acad_outcomes_by_region.csv"))
     
     # output attendance models, full
-    stargazer(reg_attend_ohc, reg_attend_n_plcmt, reg_attend_plcmt_days, 
+    stargazer(lm_attend_ohc, lm_attend_n_plcmt, lm_attend_plcmt_days, 
               type = "html",
               dep.var.labels = "Attendance Rate",
               covariate.labels = c(ohc_var_lables, control_var_labels, grade_dummy_labels),
               report = "vc*s",
-              out = paste0(p_dir_out, "reg_attendance.htm"))
+              out = paste0(p_dir_out, "lm_attendance.htm"))
 
     # output attendence models, simplified
-    stargazer(reg_attend_ohc, reg_attend_n_plcmt, reg_attend_plcmt_days, 
+    stargazer(lm_attend_ohc, lm_attend_n_plcmt, lm_attend_plcmt_days, 
               type = "html",
               dep.var.labels = "Attendance Rate",
               covariate.labels = ohc_var_lables,
               omit = omit_vars,
               report = "vc*s",
-              out = paste0(p_dir_out, "reg_attendance_condense.htm"))
+              out = paste0(p_dir_out, "lm_attendance_condense.htm"))
 
     # output removal models, full
-    stargazer(reg_remove_ohc, reg_remove_n_plcmt, reg_remove_plcmt_days, 
+    stargazer(lm_remove_ohc, lm_remove_n_plcmt, lm_remove_plcmt_days, 
               type = "html",
               dep.var.labels = "Number of Removals",
               covariate.labels = c(ohc_var_lables, control_var_labels, grade_dummy_labels),
               report = "vc*s",
-              out = paste0(p_dir_out, "reg_removal.htm"))
+              out = paste0(p_dir_out, "lm_removal.htm"))
 
     # output removal models, simplified
-    stargazer(reg_remove_ohc, reg_remove_n_plcmt, reg_remove_plcmt_days, 
+    stargazer(lm_remove_ohc, lm_remove_n_plcmt, lm_remove_plcmt_days, 
               type = "html",
               dep.var.labels = "Number of Removals",
               covariate.labels = ohc_var_lables,
               omit = omit_vars,
               report = "vc*s",
-              out = paste0(p_dir_out, "reg_removal_condense.htm"))
+              out = paste0(p_dir_out, "lm_removal_condense.htm"))
     
     # output wkce math models, full
-    stargazer(reg_kce_math_ohc, reg_kce_math_n_plcmt, reg_kce_math_plcmt_days,
+    stargazer(lm_kce_math_ohc, lm_kce_math_n_plcmt, lm_kce_math_plcmt_days,
               type = "html",
               dep.var.labels = "WKCE Score - Math (Standardized)",
               covariate.labels = c(ohc_var_lables, control_var_labels, "Grade 9"),
               report = "vc*s",
-              out = paste0(p_dir_out, "reg_wkce_math.htm"))
+              out = paste0(p_dir_out, "lm_wkce_math.htm"))
     
     # output wkce math models, simplified
-    stargazer(reg_kce_math_ohc, reg_kce_math_n_plcmt, reg_kce_math_plcmt_days,
+    stargazer(lm_kce_math_ohc, lm_kce_math_n_plcmt, lm_kce_math_plcmt_days,
               type = "html",
               dep.var.labels = "WKCE Score - Math (Standardized)",
               covariate.labels = ohc_var_lables,
               omit = omit_vars,
               report = "vc*s",
-              out = paste0(p_dir_out, "reg_wkce_math_condense.htm"))
+              out = paste0(p_dir_out, "lm_wkce_math_condense.htm"))
     
     # output wkce reading models, full
-    stargazer(reg_kce_rdg_ohc, reg_kce_rdg_n_plcmt, reg_kce_rdg_plcmt_days,
+    stargazer(lm_kce_rdg_ohc, lm_kce_rdg_n_plcmt, lm_kce_rdg_plcmt_days,
               type = "html",
               dep.var.labels = "WKCE Score - Reading (Standardized)",
               covariate.labels = c(ohc_var_lables, control_var_labels, "Grade 9"),
               report = "vc*s",
-              out = paste0(p_dir_out, "reg_wkce_rdg.htm"))
+              out = paste0(p_dir_out, "lm_wkce_rdg.htm"))
     
     # output wkce reading models, simplified
-    stargazer(reg_kce_rdg_ohc, reg_kce_rdg_n_plcmt, reg_kce_rdg_plcmt_days,
+    stargazer(lm_kce_rdg_ohc, lm_kce_rdg_n_plcmt, lm_kce_rdg_plcmt_days,
               type = "html",
               dep.var.labels = "WKCE Score - Reading (Standardized)",
               covariate.labels = ohc_var_lables,
               omit = omit_vars,
               report = "vc*s",
-              out = paste0(p_dir_out, "reg_wkce_rdg_condense.htm"))
+              out = paste0(p_dir_out, "lm_wkce_rdg_condense.htm"))
   }
   
