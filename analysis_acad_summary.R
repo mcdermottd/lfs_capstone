a1d8ba40f9066c7f3d54c90f14a1d725f5fefa5d
Drew McDermott <dmcdermott@edanalytics.org>
2016-04-28 11:13:13 -0500

    make grade updates (7 - 12)

--- analysis_acad_summary.R
+++ analysis_acad_summary.R
@@ -42,38 +42,46 @@
   # sort by academic year
   setorder(analysis_set, acad_year)
   
-  # create combined school id var
-  analysis_set[, lf_sch_id := paste0(dist_acctbl_code_cd, "_", sch_acctbl_code_cd)]
-
   # create hs subset
-  analysis_set_hs <- subset(analysis_set, flag_hs == 1)
-  
-  # create set with only 7th and 9th graders with next year test score for wkce analysis
-  analysis_set_wkce <- subset(analysis_set, (grade %in% c("07", "09") & !is.na(nxt_zscore_math_kce)))
+  sub_analysis_set <- subset(analysis_set, flag_analysis_grd == 1)
 
-##########################################
-# examine acad outcomes - summary tables #
-##########################################
+############################################
+# acad outcomes - summary tables - general #
+############################################
   
   # sort by grade
   setorder(analysis_set, grade, -flag_ohc)
 
   # calc acad outcomes, by ohc status, grade
-  a_acad_outcomes_by_grd <- analysis_set[, list(n_obs = .N,
-                                                 avg_atten = round(mean(att_rate_wi, na.rm = TRUE), 3),
-                                                 sd_atten = round(sd(att_rate_wi, na.rm = TRUE), 3),
-                                                 avg_days_remove = round(mean(days_removed_os, na.rm = TRUE), 3),
-                                                 sd_remove = round(sd(days_removed_os, na.rm = TRUE), 3),
-                                                 avg_incidents = round(mean(incidents_os, na.rm = TRUE), 3),
-                                                 sd_incidents = round(sd(incidents_os, na.rm = TRUE), 3),
-                                                 avg_math_kce = round(mean(zscore_math_kce, na.rm = TRUE), 3),
-                                                 sd_math_kce = round(sd(zscore_math_kce, na.rm = TRUE), 3),
-                                                 avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
-                                                 sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
-                                         by = c("flag_cur_plcmt", "flag_prior_plcmt", "grade")]
+  a_acad_by_grd <- analysis_set[, list(n_obs = .N,
+                                       avg_atten = round(mean(att_rate_wi, na.rm = TRUE), 3),
+                                       sd_atten = round(sd(att_rate_wi, na.rm = TRUE), 3),
+                                       avg_days_remove = round(mean(days_removed_os, na.rm = TRUE), 3),
+                                       sd_remove = round(sd(days_removed_os, na.rm = TRUE), 3),
+                                       avg_incidents = round(mean(incidents_os, na.rm = TRUE), 3),
+                                       sd_incidents = round(sd(incidents_os, na.rm = TRUE), 3),
+                                       avg_math_kce = round(mean(zscore_math_kce, na.rm = TRUE), 3),
+                                       sd_math_kce = round(sd(zscore_math_kce, na.rm = TRUE), 3),
+                                       avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
+                                       sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
+                                by = c("flag_cur_plcmt", "flag_prior_plcmt", "grade")]
   
   # calc HS acad outcomes, by ohc status
-  a_acad_outcomes <- analysis_set_hs[, list(n_obs = .N,
+  a_acad <- sub_analysis_set[, list(n_obs = .N,
+                                   avg_atten = round(mean(att_rate_wi, na.rm = TRUE), 3),
+                                   sd_atten = round(sd(att_rate_wi, na.rm = TRUE), 3),
+                                   avg_days_remove = round(mean(days_removed_os, na.rm = TRUE), 3),
+                                   sd_remove = round(sd(days_removed_os, na.rm = TRUE), 3),
+                                   avg_incidents = round(mean(incidents_os, na.rm = TRUE), 3),
+                                   sd_incidents = round(sd(incidents_os, na.rm = TRUE), 3),
+                                   avg_math_kce = round(mean(zscore_math_kce, na.rm = TRUE), 3),
+                                   sd_math_kce = round(sd(zscore_math_kce, na.rm = TRUE), 3),
+                                   avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
+                                   sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
+                            by = c("flag_cur_plcmt", "flag_prior_plcmt")]
+
+  # calc HS acad outcomes, by ohc status, gender
+  a_acad_by_gender <- sub_analysis_set[, list(n_obs = .N,
                                              avg_atten = round(mean(att_rate_wi, na.rm = TRUE), 3),
                                              sd_atten = round(sd(att_rate_wi, na.rm = TRUE), 3),
                                              avg_days_remove = round(mean(days_removed_os, na.rm = TRUE), 3),
@@ -84,36 +92,62 @@
                                              sd_math_kce = round(sd(zscore_math_kce, na.rm = TRUE), 3),
                                              avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
                                              sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
-                                     by = c("flag_cur_plcmt", "flag_prior_plcmt")]
-
-  # calc HS acad outcomes, by ohc status, placement type
-  a_acad_outcomes_by_type <- analysis_set_hs[, list(n_obs = .N,
-                                                     avg_atten = round(mean(att_rate_wi, na.rm = TRUE), 3),
-                                                     sd_atten = round(sd(att_rate_wi, na.rm = TRUE), 3),
-                                                     avg_days_remove = round(mean(days_removed_os, na.rm = TRUE), 3),
-                                                     sd_remove = round(sd(days_removed_os, na.rm = TRUE), 3),
-                                                     avg_incidents = round(mean(incidents_os, na.rm = TRUE), 3),
-                                                     sd_incidents = round(sd(incidents_os, na.rm = TRUE), 3),
-                                                     avg_math_kce = round(mean(zscore_math_kce, na.rm = TRUE), 3),
-                                                     sd_math_kce = round(sd(zscore_math_kce, na.rm = TRUE), 3),
-                                                     avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
-                                                     sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
-                                             by = c("flag_cur_plcmt", "p_type")]
+                                      by = c("flag_cur_plcmt", "flag_prior_plcmt", "d_male")]
   
   # calc HS acad outcomes, by ohc status, region
-  a_acad_outcomes_by_region <- analysis_set_hs[, list(n_obs = .N,
-                                                       avg_atten = round(mean(att_rate_wi, na.rm = TRUE), 3),
-                                                       sd_atten = round(sd(att_rate_wi, na.rm = TRUE), 3),
-                                                       avg_days_remove = round(mean(days_removed_os, na.rm = TRUE), 3),
-                                                       sd_remove = round(sd(days_removed_os, na.rm = TRUE), 3),
-                                                       avg_incidents = round(mean(incidents_os, na.rm = TRUE), 3),
-                                                       sd_incidents = round(sd(incidents_os, na.rm = TRUE), 3),
-                                                       avg_math_kce = round(mean(zscore_math_kce, na.rm = TRUE), 3),
-                                                       sd_math_kce = round(sd(zscore_math_kce, na.rm = TRUE), 3),
-                                                       avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
-                                                       sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
-                                               by = c("flag_cur_plcmt", "flag_prior_plcmt", "lf_region")]
+  a_acad_by_region <- sub_analysis_set[, list(n_obs = .N,
+                                             avg_atten = round(mean(att_rate_wi, na.rm = TRUE), 3),
+                                             sd_atten = round(sd(att_rate_wi, na.rm = TRUE), 3),
+                                             avg_days_remove = round(mean(days_removed_os, na.rm = TRUE), 3),
+                                             sd_remove = round(sd(days_removed_os, na.rm = TRUE), 3),
+                                             avg_incidents = round(mean(incidents_os, na.rm = TRUE), 3),
+                                             sd_incidents = round(sd(incidents_os, na.rm = TRUE), 3),
+                                             avg_math_kce = round(mean(zscore_math_kce, na.rm = TRUE), 3),
+                                             sd_math_kce = round(sd(zscore_math_kce, na.rm = TRUE), 3),
+                                             avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
+                                             sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
+                                      by = c("flag_cur_plcmt", "flag_prior_plcmt", "lf_region")]
+  
+###################################################
+# acad outcomes - summary tables - placement type #
+###################################################
+  
+  # calc HS acad outcomes, by ohc status, placement type
+  a_acad_by_type <- sub_analysis_set[, list(n_obs = .N,
+                                           avg_atten = round(mean(att_rate_wi, na.rm = TRUE), 3),
+                                           sd_atten = round(sd(att_rate_wi, na.rm = TRUE), 3),
+                                           avg_days_remove = round(mean(days_removed_os, na.rm = TRUE), 3),
+                                           sd_remove = round(sd(days_removed_os, na.rm = TRUE), 3),
+                                           avg_incidents = round(mean(incidents_os, na.rm = TRUE), 3),
+                                           sd_incidents = round(sd(incidents_os, na.rm = TRUE), 3),
+                                           avg_math_kce = round(mean(zscore_math_kce, na.rm = TRUE), 3),
+                                           sd_math_kce = round(sd(zscore_math_kce, na.rm = TRUE), 3),
+                                           avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
+                                           sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
+                                    by = c("flag_cur_plcmt", "p_type")]
+  
 
+  
+  # calc HS acad outcomes, by ohc status, gender and placement type
+  a_acad_by_gender_type <- sub_analysis_set[, list(n_obs = .N,
+                                                   avg_atten = round(mean(att_rate_wi, na.rm = TRUE), 3),
+                                                   sd_atten = round(sd(att_rate_wi, na.rm = TRUE), 3),
+                                                   avg_days_remove = round(mean(days_removed_os, na.rm = TRUE), 3),
+                                                   sd_remove = round(sd(days_removed_os, na.rm = TRUE), 3),
+                                                   avg_incidents = round(mean(incidents_os, na.rm = TRUE), 3),
+                                                   sd_incidents = round(sd(incidents_os, na.rm = TRUE), 3),
+                                                   avg_math_kce = round(mean(zscore_math_kce, na.rm = TRUE), 3),
+                                                   sd_math_kce = round(sd(zscore_math_kce, na.rm = TRUE), 3),
+                                                   avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3),
+                                                   sd_rdg_kce = round(sd(zscore_rdg_kce, na.rm = TRUE), 3)),
+                                           by = c("flag_cur_plcmt", "flag_prior_plcmt", "d_male", "p_type")]
+  
+  # calc wkce scores by grade (8 and 10), gender, and placement type
+  a_wkce_grade_gender_type <- sub_analysis_set[grade == "08" | grade == "10" , list(n_obs = .N,
+                                                                                   avg_math_kce = round(mean(zscore_math_kce, na.rm = TRUE), 3),
+                                                                                   avg_rdg_kce = round(mean(zscore_rdg_kce, na.rm = TRUE), 3)),
+                                              by = c("flag_cur_plcmt", "flag_prior_plcmt", "grade", "d_male", "p_type")]
+  
 #####################
 # format and export #
 #####################
@@ -125,10 +159,13 @@
   if (p_opt_exp == 1) { 
     
     # output outcome descriptive tables
-    ea_write(a_acad_outcomes, paste0(p_dir_out, "acad_outcomes_overall_hs.csv"))
-    ea_write(a_acad_outcomes_by_grd, paste0(p_dir_out, "acad_outcomes_by_grd.csv"))
-    ea_write(a_acad_outcomes_by_type, paste0(p_dir_out, "acad_outcomes_by_type.csv"))
-    ea_write(a_acad_outcomes_by_region, paste0(p_dir_out, "acad_outcomes_by_region.csv"))
+    ea_write(a_acad, paste0(p_dir_out, "acad_outcomes_overall_hs.csv"))
+    ea_write(a_acad_by_grd, paste0(p_dir_out, "acad_outcomes_by_grd.csv"))
+    ea_write(a_acad_by_type, paste0(p_dir_out, "acad_outcomes_by_type.csv"))
+    ea_write(a_acad_by_region, paste0(p_dir_out, "acad_outcomes_by_region.csv"))
+    ea_write(a_acad_by_gender, paste0(p_dir_out, "acad_outcomes_by_gender.csv"))
+    ea_write(a_acad_by_gender_type, paste0(p_dir_out, "acad_outcomes_by_gender_type.csv"))
+    ea_write(a_wkce_grade_gender_type, paste0(p_dir_out, "wkce_by_grade_gender_type.csv"))
     
   }
   
