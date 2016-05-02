3150166fc8841885f322d626340b12daa9f910fb
Drew McDermott <dmcdermott@edanalytics.org>
2016-05-02 12:54:10 -0500

    adjust ptype regressions (remove other)

--- analysis_acad_regress.R
+++ analysis_acad_regress.R
@@ -137,16 +137,32 @@
   lm_controls_full <- paste(c(lm_student_controls, lm_dummies_grade, lm_sch_controls, lm_dummies_yr), collapse = " + ")
   lm_controls_wkce <- paste(c(lm_student_controls, "d_grade_09", lm_sch_controls, lm_dummies_yr), collapse = " + ")
 
+#####################################################
+# create set without missings to use in regressions #
+#####################################################
+  
+  # create dataset with no missings
+  attend_set <- subset(analysis_sample, select = c("att_rate_wi", lm_var_list))
+  removal_set <- subset(analysis_sample, select = c("days_removed_os", lm_var_list))
+  wkce_math_set <- subset(analysis_sample, select = c("nxt_zscore_math_kce", lm_var_list))
+  wkce_rdg_set <- subset(analysis_sample, select = c("nxt_zscore_rdg_kce", lm_var_list))
+
+  # remove all missings to run clustering function #brule
+  attend_set <- na.omit(attend_set)
+  removal_set <- na.omit(removal_set)
+  wkce_math_set <- na.omit(wkce_math_set)
+  wkce_rdg_set <- na.omit(wkce_rdg_set)
+  
+  # remove ptype "other" for ptype regressions
+  attend_set_ptype <- subset(attend_set, d_p_type_other != 1)
+  removal_set_ptype <- subset(removal_set, d_p_type_other != 1)
+  wkce_math_set_ptype <- subset(wkce_math_set, d_p_type_other != 1)
+  wkce_rdg_set_ptype <- subset(wkce_rdg_set, d_p_type_other != 1)
+
 ############################
 # regressions - attendence #
 ############################
 
-  # create dataset with no missings
-  attend_set <- subset(analysis_sample, select = c("att_rate_wi", lm_var_list))
-  
-  # remove all missings to run clustering function #brule
-  attend_set <- na.omit(attend_set)
-  
   # reg: attendance on OHC flags
   lm_formula <- paste("att_rate_wi ~ flag_cur_plcmt + flag_prior_plcmt + ", lm_controls_full)
   m1a_attend_ohc <- lm(lm_formula, data = attend_set)
@@ -160,32 +176,26 @@
   m1c_attend_pdays_log <- lm(lm_formula, data = attend_set)
 
   # reg: attendance on type of OHC
-  lm_formula <- paste("att_rate_wi ~ d_p_type_fhome_rel + d_p_type_fhome_nonrel + d_p_type_group_home + d_p_type_rcc + d_p_type_other + 
-                      flag_prior_plcmt + ", lm_controls_full)
-  m1d_attend_ptype <- lm(lm_formula, data = attend_set)
+  lm_formula <- paste("att_rate_wi ~ d_p_type_fhome_rel + d_p_type_fhome_nonrel + d_p_type_group_home + d_p_type_rcc + flag_prior_plcmt + ", 
+                      lm_controls_full)
+  m1d_attend_ptype <- lm(lm_formula, data = attend_set_ptype)
   
   # reg: attendance on region
-  lm_formula <- paste("att_rate_wi ~ int_reg_mke_ohc + int_reg_se_ohc + int_reg_s_ohc + int_reg_w_ohc + int_reg_nw_ohc + int_reg_nc_ohc + 
-                      int_reg_ne_ohc + flag_prior_plcmt + ", lm_controls_full)
+  lm_formula <- paste("att_rate_wi ~ int_reg_nc_ohc + int_reg_ne_ohc + int_reg_mke_ohc + int_reg_se_ohc + int_reg_s_ohc + int_reg_w_ohc +
+                      int_reg_nw_ohc + flag_prior_plcmt + ", lm_controls_full)
   m1e_attend_reg <- lm(lm_formula, data = attend_set)
   
   # append var-cov matrix with clustered standard errors as additional model argumented 
   m1a_attend_ohc$se <- func_cl_vcov(m1a_attend_ohc, attend_set$lf_sch_id)
   m1b_attend_plcmts_log$se <- func_cl_vcov(m1b_attend_plcmts_log, attend_set$lf_sch_id)
   m1c_attend_pdays_log$se <- func_cl_vcov(m1c_attend_pdays_log, attend_set$lf_sch_id)
-  m1d_attend_ptype$se <- func_cl_vcov(m1d_attend_ptype, attend_set$lf_sch_id)
+  m1d_attend_ptype$se <- func_cl_vcov(m1d_attend_ptype, attend_set_ptype$lf_sch_id)
   m1e_attend_reg$se <- func_cl_vcov(m1e_attend_reg, attend_set$lf_sch_id)
 
 ##########################
 # regressions - removals #
 ##########################
-  
-  # create dataset with no missings
-  removal_set <- subset(analysis_sample, select = c("days_removed_os", lm_var_list))
-  
-  # remove all missings to run clustering function #brule
-  removal_set <- na.omit(removal_set)
-  
+
   # reg: removals on OHC flags
   lm_formula <- paste("days_removed_os ~ flag_cur_plcmt + flag_prior_plcmt + ", lm_controls_full)
   m2a_remove_ohc <- lm(lm_formula, data = removal_set)
@@ -199,32 +209,26 @@
   m2c_remove_pdays_log <- lm(lm_formula, data = removal_set)
 
   # reg: removals on type of OHC
-  lm_formula <- paste("days_removed_os ~ d_p_type_fhome_rel + d_p_type_fhome_nonrel + d_p_type_group_home + d_p_type_rcc + d_p_type_other + 
-                      flag_prior_plcmt + ", lm_controls_full)
-  m2d_remove_ptype <- lm(lm_formula, data = removal_set)
+  lm_formula <- paste("days_removed_os ~ d_p_type_fhome_rel + d_p_type_fhome_nonrel + d_p_type_group_home + d_p_type_rcc + flag_prior_plcmt + ",
+                      lm_controls_full)
+  m2d_remove_ptype <- lm(lm_formula, data = removal_set_ptype)
   
   # reg: removals on region
-  lm_formula <- paste("days_removed_os ~ int_reg_mke_ohc + int_reg_se_ohc + int_reg_s_ohc + int_reg_w_ohc + int_reg_nw_ohc + int_reg_nc_ohc + 
-                      int_reg_ne_ohc + flag_prior_plcmt + ", lm_controls_full)
+  lm_formula <- paste("days_removed_os ~ int_reg_nc_ohc + int_reg_ne_ohc + int_reg_mke_ohc + int_reg_se_ohc + int_reg_s_ohc + int_reg_w_ohc +
+                      int_reg_nw_ohc + flag_prior_plcmt + ", lm_controls_full)
   m2e_remove_reg <- lm(lm_formula, data = removal_set)
   
   # append var-cov matrix with clustered standard errors as additional model argumented 
   m2a_remove_ohc$se <- func_cl_vcov(m2a_remove_ohc, removal_set$lf_sch_id)
   m2b_remove_plcmts_log$se <- func_cl_vcov(m2b_remove_plcmts_log, removal_set$lf_sch_id)
   m2c_remove_pdays_log$se <- func_cl_vcov(m2c_remove_pdays_log, removal_set$lf_sch_id)
-  m2d_remove_ptype$se <- func_cl_vcov(m2d_remove_ptype, removal_set$lf_sch_id)
+  m2d_remove_ptype$se <- func_cl_vcov(m2d_remove_ptype, removal_set_ptype$lf_sch_id)
   m2e_remove_reg$se <- func_cl_vcov(m2e_remove_reg, removal_set$lf_sch_id)
 
 ###########################
 # regressions - wkce math #
 ###########################
-  
-  # create dataset with no missings
-  wkce_math_set <- subset(analysis_sample, select = c("nxt_zscore_math_kce", lm_var_list))
-  
-  # remove all missings to run clustering function #brule
-  wkce_math_set <- na.omit(wkce_math_set)
-  
+
   # reg: math wkce on OHC flags
   lm_formula <- paste("nxt_zscore_math_kce ~ flag_cur_plcmt + flag_prior_plcmt + ", lm_controls_wkce)
   m3a_wkce_math_ohc <- lm(lm_formula, data = wkce_math_set)
@@ -238,32 +242,26 @@
   m3c_wkce_math_pdays_log <- lm(lm_formula, data = wkce_math_set)
 
   # reg: math wkce on type of OHC
-  lm_formula <- paste("nxt_zscore_math_kce ~ d_p_type_fhome_rel + d_p_type_fhome_nonrel + d_p_type_group_home + d_p_type_rcc + d_p_type_other + 
-                      flag_prior_plcmt + ", lm_controls_wkce)
-  m3d_wkce_math_ptype <- lm(lm_formula, data = wkce_math_set)
+  lm_formula <- paste("nxt_zscore_math_kce ~ d_p_type_fhome_rel + d_p_type_fhome_nonrel + d_p_type_group_home + d_p_type_rcc + flag_prior_plcmt + ",
+                      lm_controls_wkce)
+  m3d_wkce_math_ptype <- lm(lm_formula, data = wkce_math_set_ptype)
   
   # reg: math wkce on region
-  lm_formula <- paste("nxt_zscore_math_kce ~ int_reg_mke_ohc + int_reg_se_ohc + int_reg_s_ohc + int_reg_w_ohc + int_reg_nw_ohc + int_reg_nc_ohc + 
-                      int_reg_ne_ohc + flag_prior_plcmt + ", lm_controls_wkce)
+  lm_formula <- paste("nxt_zscore_math_kce ~ int_reg_nc_ohc + int_reg_ne_ohc + int_reg_mke_ohc + int_reg_se_ohc + int_reg_s_ohc + int_reg_w_ohc +
+                      int_reg_nw_ohc + flag_prior_plcmt + ", lm_controls_wkce)
   m3e_wkce_math_reg <- lm(lm_formula, data = wkce_math_set)
   
   # append var-cov matrix with clustered standard errors as additional model argumented 
   m3a_wkce_math_ohc$se <- func_cl_vcov(m3a_wkce_math_ohc, wkce_math_set$lf_sch_id)
   m3b_wkce_math_plcmts_log$se <- func_cl_vcov(m3b_wkce_math_plcmts_log, wkce_math_set$lf_sch_id)
   m3c_wkce_math_pdays_log$se <- func_cl_vcov(m3c_wkce_math_pdays_log, wkce_math_set$lf_sch_id)
-  m3d_wkce_math_ptype$se <- func_cl_vcov(m3d_wkce_math_ptype, wkce_math_set$lf_sch_id)
+  m3d_wkce_math_ptype$se <- func_cl_vcov(m3d_wkce_math_ptype, wkce_math_set_ptype$lf_sch_id)
   m3e_wkce_math_reg$se <- func_cl_vcov(m3e_wkce_math_reg, wkce_math_set$lf_sch_id)
-
+  
 ##############################
 # regressions - wkce reading #
 ##############################
-  
-  # create dataset with no missings
-  wkce_rdg_set <- subset(analysis_sample, select = c("nxt_zscore_rdg_kce", lm_var_list))
-  
-  # remove all missings to run clustering function #brule
-  wkce_rdg_set <- na.omit(wkce_rdg_set)
-  
+
   # reg: reading wkce on OHC flags
   lm_formula <- paste("nxt_zscore_rdg_kce ~ flag_cur_plcmt + flag_prior_plcmt + ", lm_controls_wkce)
   m4a_wkce_rdg_ohc <- lm(lm_formula, data = wkce_rdg_set)
@@ -277,9 +275,9 @@
   m4c_wkce_rdg_pdays_log <- lm(lm_formula, data = wkce_rdg_set)
 
   # reg: reading wkce on type of OHC
-  lm_formula <- paste("nxt_zscore_rdg_kce ~ d_p_type_fhome_rel + d_p_type_fhome_nonrel + d_p_type_group_home + d_p_type_rcc + d_p_type_other + 
-                      flag_prior_plcmt + ", lm_controls_wkce)
-  m4d_wkce_rdg_ptype <- lm(lm_formula, data = wkce_rdg_set)
+  lm_formula <- paste("nxt_zscore_rdg_kce ~ d_p_type_fhome_rel + d_p_type_fhome_nonrel + d_p_type_group_home + d_p_type_rcc + flag_prior_plcmt + ",
+                      lm_controls_wkce)
+  m4d_wkce_rdg_ptype <- lm(lm_formula, data = wkce_rdg_set_ptype)
   
   # reg: reading wkce on region
   lm_formula <- paste("nxt_zscore_rdg_kce ~ int_reg_mke_ohc + int_reg_se_ohc + int_reg_s_ohc + int_reg_w_ohc + int_reg_nw_ohc + int_reg_nc_ohc + 
@@ -290,7 +288,7 @@
   m4a_wkce_rdg_ohc$se <- func_cl_vcov(m4a_wkce_rdg_ohc, wkce_rdg_set$lf_sch_id)
   m4b_wkce_rdg_plcmts_log$se <- func_cl_vcov(m4b_wkce_rdg_plcmts_log, wkce_rdg_set$lf_sch_id)
   m4c_wkce_rdg_pdays_log$se <- func_cl_vcov(m4c_wkce_rdg_pdays_log, wkce_rdg_set$lf_sch_id)
-  m4d_wkce_rdg_ptype$se <- func_cl_vcov(m4d_wkce_rdg_ptype, wkce_rdg_set$lf_sch_id)
+  m4d_wkce_rdg_ptype$se <- func_cl_vcov(m4d_wkce_rdg_ptype, wkce_rdg_set_ptype$lf_sch_id)
   m4e_wkce_rdg_reg$se <- func_cl_vcov(m4e_wkce_rdg_reg, wkce_rdg_set$lf_sch_id)
 
 ######################
@@ -382,7 +380,7 @@
                   Sweave = FALSE,
                   float = "longtable",
                   coef.names = c("(Intercept)", paste0("Placement Type: ", c("Foster Home - Relative", "Foster Home - Non-Relative", "Group Home",
-                                                                             "RCC", "Other")), 
+                                                                             "RCC")), 
                                  "Placement in Prior Academic Year", student_control_labels, grade_dummy_labels, school_control_labels, 
                                  acad_year_labels),
                   label = "ptype_models",
@@ -395,8 +393,8 @@
                   model.names = c("Attendance Rate", "Removals", "WKCE Math Score", "WKCE Reading Score"),
                   Sweave = FALSE,
                   float = "longtable",
-                  coef.names = c("(Intercept)", paste0("Region: ", c("Milwaukee", "Southeast", "South", "West", "Northwest", "Northcentral", 
-                                                                     "Northeast")),
+                  coef.names = c("(Intercept)", paste0("Region ", c("1 (Northcentral)","2 (Northeast)", "3 (Milwaukee)", "4 (Southeast)", "5 (South)",
+                                                                    "6 (West)", "7 (Northwest)")),
                                  "Placement in Prior Academic Year", student_control_labels, grade_dummy_labels, school_control_labels, 
                                  acad_year_labels),
                   label = "region_models",
@@ -404,77 +402,7 @@
         file = paste0(p_dir_out, "models_region.tex"))
     
   }
-    
-    
-    int_reg_mke_ohc + int_reg_se_ohc + int_reg_s_ohc + int_reg_w_ohc + int_reg_nw_ohc + int_reg_nc_ohc + 
-                      int_reg_ne_ohc + flag_prior_plcmt 
+
   
-  stargazer(lm_attend_ohc, lm_attend_n_plcmt, lm_attend_plcmt_days, lm_attend_ptype, 
-              type = "html",
-              dep.var.labels = "Attendance Rate",
-              covariate.labels = c(ohc_var_lables, control_var_labels, grade_dummy_labels),
-              report = "vc*s",
-              out = paste0(p_dir_out, "lm_attendance.htm"))
-
-    # output attendence models, simplified
-    stargazer(lm_attend_ohc, lm_attend_n_plcmt, lm_attend_plcmt_days, 
-              type = "html",
-              dep.var.labels = "Attendance Rate",
-              covariate.labels = ohc_var_lables,
-              omit = omit_vars,
-              report = "vc*s",
-              out = paste0(p_dir_out, "lm_attendance_condense.htm"))
-
-    # output removal models, full
-    stargazer(lm_remove_ohc, lm_remove_n_plcmt, lm_remove_plcmt_days, 
-              type = "html",
-              dep.var.labels = "Number of Removals",
-              covariate.labels = c(ohc_var_lables, control_var_labels, grade_dummy_labels),
-              report = "vc*s",
-              out = paste0(p_dir_out, "lm_removal.htm"))
-
-    # output removal models, simplified
-    stargazer(lm_remove_ohc, lm_remove_n_plcmt, lm_remove_plcmt_days, 
-              type = "html",
-              dep.var.labels = "Number of Removals",
-              covariate.labels = ohc_var_lables,
-              omit = omit_vars,
-              report = "vc*s",
-              out = paste0(p_dir_out, "lm_removal_condense.htm"))
-    
-    # output wkce math models, full
-    stargazer(lm_kce_math_ohc, lm_kce_math_n_plcmt, lm_kce_math_plcmt_days,
-              type = "html",
-              dep.var.labels = "WKCE Score - Math (Standardized)",
-              covariate.labels = c(ohc_var_lables, control_var_labels, "Grade 9"),
-              report = "vc*s",
-              out = paste0(p_dir_out, "lm_wkce_math.htm"))
-    
-    # output wkce math models, simplified
-    stargazer(lm_kce_math_ohc, lm_kce_math_n_plcmt, lm_kce_math_plcmt_days,
-              type = "html",
-              dep.var.labels = "WKCE Score - Math (Standardized)",
-              covariate.labels = ohc_var_lables,
-              omit = omit_vars,
-              report = "vc*s",
-              out = paste0(p_dir_out, "lm_wkce_math_condense.htm"))
-    
-    # output wkce reading models, full
-    stargazer(lm_kce_rdg_ohc, lm_kce_rdg_n_plcmt, lm_kce_rdg_plcmt_days,
-              type = "html",
-              dep.var.labels = "WKCE Score - Reading (Standardized)",
-              covariate.labels = c(ohc_var_lables, control_var_labels, "Grade 9"),
-              report = "vc*s",
-              out = paste0(p_dir_out, "lm_wkce_rdg.htm"))
-    
-    # output wkce reading models, simplified
-    stargazer(lm_kce_rdg_ohc, lm_kce_rdg_n_plcmt, lm_kce_rdg_plcmt_days,
-              type = "html",
-              dep.var.labels = "WKCE Score - Reading (Standardized)",
-              covariate.labels = ohc_var_lables,
-              omit = omit_vars,
-              report = "vc*s",
-              out = paste0(p_dir_out, "lm_wkce_rdg_condense.htm"))
-    
-    
+  
     
